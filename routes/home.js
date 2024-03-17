5af453dbec83412079f706baf5f3f9f984123fed
inputvar <varunkargathara67@gmail.com>
2024-03-17 09:57:06 +0530

    safe ui

--- routes/home.js
+++ routes/home.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req,res) => {
-	res.render("home/welcome");
+router.get("/", (req, res) => {
+  res.render("home/welcome");
 });
 
-router.get("/home/about-us", (req,res) => {
-	res.render("home/aboutUs", { title: "About Us | HungerHelper" });
+router.get("/home/about-us", (req, res) => {
+  res.render("home/aboutUs", { title: "About Us | FOOD200" });
 });
 
-router.get("/home/mission", (req,res) => {
-	res.render("home/mission", { title: "Our mission | HungerHelper" });
+router.get("/home/mission", (req, res) => {
+  res.render("home/mission", { title: "Our mission | FOOD200" });
 });
 
-router.get("/home/contact-us", (req,res) => {
-	res.render("home/contactUs", { title: "Contact us | HungerHelper" });
+router.get("/home/contact-us", (req, res) => {
+  res.render("home/contactUs", { title: "Contact us | FOOD200" });
 });
-
 
 module.exports = router;
